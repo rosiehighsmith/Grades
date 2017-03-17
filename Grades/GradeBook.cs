ab3c47a0e1dff6613ee1c6d51a86d5fee57135f4
rosiehighsmith <ariel.highsmith@gmail.com>
2017-03-17 07:20:55 -0700

    Added switch and for loop.

--- Grades/GradeBook.cs
+++ Grades/GradeBook.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -28,6 +29,14 @@ namespace Grades
             }
             stats.AverageGrade = sum / grades.Count;
             return stats;
+        }
+
+        public void WriteGrades(TextWriter destination)
+        {
+            for (int i = 0; i < grades.Count; i++)
+            {
+                destination.WriteLine(grades[i]);
+            }
         }
 
         public void AddGrade(float grade)
--- Grades/GradeStatistics.cs
+++ Grades/GradeStatistics.cs
@@ -13,7 +13,32 @@ namespace Grades
             HighestGrade = 0;
             LowestGrade = float.MaxValue;
         }
-
+        public string Description
+        {
+            get
+            {
+                string result;
+                switch (LetterGrade)
+                {
+                    case "A":
+                        result = "Excellent";
+                        break;
+                    case "B":
+                        result = "Good";
+                        break;
+                    case "C":
+                        result = "Average";
+                        break;
+                    case "D":
+                        result = "Below average";
+                        break;
+                    default:
+                        result = "Failing";
+                        break;
+                }
+                return result;
+            }
+        }
         public string LetterGrade
         {
             get
